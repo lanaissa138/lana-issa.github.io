78afd18a9e178a731d9ebfaaf215c54a12f1381b
lanaissa138 <lanaissa@my.yorku.ca>
2025-07-19 20:05:00 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
       posts.forEach(post => {
         const postElement = document.createElement('div');
         postElement.classList.add('blog-post');
-      
+          <h2>${post.title}</h2>
+          <p><em>${post.date}</em></p>
+          <p>${post.content}</p>
+        ;
+
+        blogList.appendChild(postElement);
       });
     })
   .catch(error => console.error('Error loading blog posts:', error));
