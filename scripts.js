f282a42a62a999b7db0aae4c0c913b756b9041d7
lanaissa138 <lanaissa@my.yorku.ca>
2025-07-19 20:12:27 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,11 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
       posts.forEach(post => {
         const postElement = document.createElement('div');
         postElement.classList.add('blog-post');
+
+        postElement.innerHTML = `
           <h2>${post.title}</h2>
           <p><em>${post.date}</em></p>
           <p>${post.content}</p>
-        ;
-
+  `      ;
+        
         blogList.appendChild(postElement);
       });
     })
